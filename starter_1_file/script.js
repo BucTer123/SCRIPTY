3545ce15d430dbaa3d703eb4a1e9ca50e4f1746b
BucTer123 <bambukvova3@gmail.com>
2025-11-27 19:33:35 +0200

    starter web

---
+++ starter_1_file/script.js
@@ -0,0 +1,13 @@
+document.getElementById("button1").addEventListener("click", function() {
+    alert("Yes! this is interesting !");
+})
+document.getElementById("button2").addEventListener("click", function() {
+    alert("Okay but i love this!");
+})
+document.getElementById("Welcome_input").addEventListener("I love programing", function() {
+    alert("I so!");
+})
+document.getElementById("Welcome_input").addEventListener("I hate programing", function(){
+    alert("okay but i love programing if you want know ;( ");
+})
+
