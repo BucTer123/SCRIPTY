bbd840c1f07460dd7b92e3430c5b45fd4f263932
BucTer123 <bambukvova3@gmail.com>
2025-11-27 19:33:21 +0200

    websites

---
+++ website_answer/js.js
@@ -0,0 +1,15 @@
+document.getElementById("button_yes1").addEventListener("click", function(){
+    alert("Понятно");
+})
+document.getElementById("button_yes2").addEventListener("click", function(){
+    alert("Понятно");
+})
+document.getElementById("button_div_yes").addEventListener("click", function(){
+    alert("Понятно");
+})
+document.getElementById("button_div_yes2").addEventListener("click", function(){
+    alert("Понятно");
+})
+
+
+
