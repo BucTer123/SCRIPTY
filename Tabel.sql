3b103981376a02c99d3c2ab4c856306e021324c4
BucTer123 <bambukvova3@gmail.com>
2025-11-28 17:25:21 +0200

    Tabel

---
+++ Tabel.sql
@@ -0,0 +1,17 @@
+CREATE TABLE govno (
+	id INT,
+	youtubers TEXT,
+)
+INSERT INTO govno(id, youtubers)
+VALUES
+	(1, 'слєш'),
+	(2, 'PewDiePie'),
+	(3, 'kussia88'),
+	(4, 'stintik'),
+	(5, 'планя'),
+	(6, 'xKamysh'),
+	(7, 'Foxline'),
+	(8, 'OneGo'),
+	(9, 'T2x2'),
+	(10, 'MrLololoshka(Роман Фильченков)' ),
+CREATE USER user_1
