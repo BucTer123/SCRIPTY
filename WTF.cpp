dc7e02a671236898f5a33c12053d0a162f3e2cee
BucTer123 <bambukvova3@gmail.com>
2025-11-28 17:27:27 +0200

    Add files via upload

---
+++ WTF.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string femboy;
+    cout<<"Welcome!\n";
+    cout<<"Are you a femboy      :";
+    cin>>femboy;
+    if (femboy == "yes") {
+    	cout<<"А ти пидорас или педофил? ";
+    }
+    if (femboy == "no") {
+        cout<<"Пиздато!!";
+    }
+}
+
+
