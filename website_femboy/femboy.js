1b4f79b129710e11d08561c3ae81675fe39477bc
BucTer123 <bambukvova3@gmail.com>
2025-11-29 12:36:51 +0200

    website femboy

---
+++ website_femboy/femboy.js
@@ -0,0 +1,12 @@
+document.getElementById("yes").addEventListener("click", function() {
+    alert("Хорошо что ти фембой ))) я смогу почуствовать себя лучше с тобой");
+})
+document.getElementById("no").addEventListener("click", function(){
+    alert("Окей!");
+})
+document.getElementById("quit").addEventListener("click", function(){
+    alert("Виход");
+    return 0;
+})
+
+
