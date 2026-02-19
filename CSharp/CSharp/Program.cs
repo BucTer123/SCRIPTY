b57780965ebb4f8535fd5d664a6415354fa0191b
SHALAWA <bambukvova3@gmail.com>
2026-02-19 15:28:43 +0100

    Update Program.cs

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -1,1 +1,19 @@
-﻿
+using System;
+using System.IO;
+using System.WinForms
+using System.Drawing
+
+void main() {
+  Form win = new Form();
+  win text = "window";
+  win Size = new Size(640, 400);
+
+  Button btn = new Button();
+  btn text = "Click button";
+  btn Location = new Point(130, 100);
+
+  btn.CLick - (sender, e) => 
+  {
+    MessageBox("IDI NAHU");
+  }
+}
